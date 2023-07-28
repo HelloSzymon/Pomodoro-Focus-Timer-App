8818bf8a8c16399331258f66e20d0180b764bd45
Szymon Wnuk <101000022+HelloSzymon@users.noreply.github.com>
2023-07-28 13:14:10 +0200

    Initial Commit

---
+++ Pomodoro Focus Timer App/Pomodoro_Focus_Timer_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro_Focus_Timer_AppApp.swift
+//  Pomodoro Focus Timer App
+//
+//  Created by Szymon Wnuk on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_Focus_Timer_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
